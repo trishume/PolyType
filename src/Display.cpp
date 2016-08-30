0c764911a9019af64ae18ce68bc396fa52f97a83
Tristan Hume <tris.hume@gmail.com>
2016-08-29 20:00:44 -0400

    Test LEDs

--- src/Display.cpp
+++ src/Display.cpp
@@ -2,9 +2,14 @@
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
+#include <Adafruit_NeoPixel.h>
 
 #include "Info.h"
 
+#define LED_PIN   12
+#define NUMPIXELS  3
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 static Adafruit_PCD8544 theOnlyDisplay = Adafruit_PCD8544(14, 16, 15);
 const char *defaultHeader = "- PolyType -";
 
@@ -20,12 +25,19 @@ void Display::setup() {
   display->setTextSize(1);
   display->setTextColor(BLACK);
   display->setCursor(0,0);
+  pixels.begin();
+  // pinMode(LED_PIN, OUTPUT);
 }
 
 void Display::render() {
+  // pinMode(LED_PIN, OUTPUT);
+  // digitalWrite(LED_PIN, LOW);
+  // delay(3000);
+  pixels.setPixelColor(0, pixels.Color(0,0,150));
+  pixels.setPixelColor(1, pixels.Color(0,0,150));
+  pixels.show();
   if(dirty == 0) return;
   dirty = 0;
-
   display->clearDisplay();
   if(!sleeping) {
     display->println(header);
